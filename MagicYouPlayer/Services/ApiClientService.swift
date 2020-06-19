70e3d1db83fa98471ee98a863f65707d52d19228
xredlinex <redik.line@gmail.com>
2020-06-19 14:10:49 +0300

    service

---
+++ MagicYouPlayer/Services/ApiClientService.swift
@@ -0,0 +1,9 @@
+//
+//  ApiClientService.swift
+//  MagicYouPlayer
+//
+//  Created by alexey sorochan on 19.06.2020.
+//  Copyright © 2020 alexey sorochan. All rights reserved.
+//
+
+import Foundation
