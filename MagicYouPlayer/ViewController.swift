44dc03fcf441294e3169ea2bcc340ddfec75b708
xredlinex <redik.line@gmail.com>
2020-06-19 13:33:58 +0300

    initial commit

---
+++ MagicYouPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagicYouPlayer
+//
+//  Created by alexey sorochan on 19.06.2020.
+//  Copyright © 2020 alexey sorochan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
