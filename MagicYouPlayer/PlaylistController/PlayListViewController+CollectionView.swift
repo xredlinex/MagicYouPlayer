f245e56f47cbecc0cd2660e98c42cb39a1109a4a
xredlinex <redik.line@gmail.com>
2020-06-24 14:11:07 +0300

    fix some flow layout collection and some ui elemtnth

--- MagicYouPlayer/PlaylistController/PlayListViewController+CollectionView.swift
+++ MagicYouPlayer/PlaylistController/PlayListViewController+CollectionView.swift
@@ -81,7 +81,7 @@ extension PlayListViewController: UICollectionViewDelegate, UICollectionViewData
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
         if collectionView == favoritePlaylistCollectionview {
-            return CGSize(width: collectionView.frame.width / 2, height: collectionView.frame.width / 2)
+            return CGSize(width: collectionView.frame.width / 3 - 5, height: collectionView.frame.height)
         } else if collectionView == playlistCollectionView {
             return CGSize(width: collectionView.frame.width / 2.3, height: collectionView.frame.height)
         } else {
